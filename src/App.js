ac3ce62dd5deabd3b8d5f189806a640bb7999321
Emma Bindi <emma.bindi@outlook.com>
2020-07-07 08:51:02 +1000

    App component setup pull of data from currencies file to create dropdown choices list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, {useState} from 'react'
+import currencies from "./supported-currencies.json";
 
 const App = () => {
   const defaultCurrency = "AUD";
   const [currency, setCurrency] = useState(defaultCurrency);
+  const [bitcoinData, setBitcoinData] = useState({});
+
+  const onOptionChange = (event) => {
+    setCurrency(event.target.value);
+  }
+
   return (
     <div >
-          The basic empty template
+      <span>Select your currency: </span>
+      <select value={currency} onChange={onOptionChange}>
+      {currencies.map((obj, index)=> (
+        <option key={`${index}-${obj.country}`} value={obj.currency}>{obj.country}</option>
+      ))}
+      </select>
+      <h1>Bitcoin Data for {currency}</h1>
+      {Object.keys(bitcoinData).map((date) => (
+        <div key={date}>
+          Date: {date} Value: {bitcoinData[date]}
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default App

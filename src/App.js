c3313056db18008eed3f855c9c5c633dc3d620da
Emma Bindi <emma.bindi@outlook.com>
2020-07-07 08:42:51 +1000

    Initial commit currency json file saved in SRC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+  const defaultCurrency = "AUD";
+  const [currency, setCurrency] = useState(defaultCurrency);
   return (
     <div >
           The basic empty template
